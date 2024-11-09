208b9d8f0ac2f9b1cf3ca709d700042bd961e6f4
Tighttempo <riseanddeliver@protonmail.com>
2024-11-09 22:57:01 +0100

    Quartz sync: Nov 9, 2024, 10:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "www.nicolargi.fr",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
