ec1026fccdab05d2051c7fb58726f3382c1a5ccd
Tighttempo <riseanddeliver@protonmail.com>
2024-11-09 22:57:54 +0100

    Quartz sync: Nov 9, 2024, 10:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🦧 La Grimace Du Singe",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
